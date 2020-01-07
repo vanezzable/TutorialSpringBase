d20ccfaab2c7706e3cb36b8a28aa7fac153e0ea6
Ivan Maltese <ivan.maltese@objectmethod.it>
2020-01-07 13:08:09 +0100

    format code

--- src/it/objectmethod/tutorial/model/Country.java
+++ src/it/objectmethod/tutorial/model/Country.java
@@ -4,18 +4,21 @@ public class Country {
 
 	private String codice;
 	private String nome;
-	
+
 	public String getCodice() {
 		return codice;
 	}
+
 	public void setCodice(String codice) {
 		this.codice = codice;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+
 }
